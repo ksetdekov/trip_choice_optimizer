bcc5b0657e1225ab158eadc4edb8ba5a80816746
ksetdekov <ksetdekov@gmail.com>
2021-09-26 21:57:54 +0300

    updated structure for heroku

--- main_my.py
+++ main_my.py
@@ -12,6 +12,7 @@ if 'bot_token' in os.environ:
 else:
     print('getting token from file')
     import config_my
+
     bot_token = config_my.token
 
 bot = telebot.TeleBot(bot_token)
@@ -32,31 +33,32 @@ def echo(message):
     bot.send_message(message.chat.id, 'ответ')
 
 
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
+if __name__ == '__main__':
+    # Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+    if "HEROKU" in list(os.environ.keys()):
+        logger = telebot.logger
+        telebot.logger.setLevel(logging.INFO)
 
-    server = Flask(__name__)
-
-
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+        server = Flask(__name__)
 
 
-    @server.route("/")
-    def webhook():
+        @server.route("/bot", methods=['POST'])
+        def getMessage():
+            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
+
+
+        @server.route("/")
+        def webhook():
+            bot.remove_webhook()
+            bot.set_webhook(
+                url="https://choice-optimizer.herokuapp.com")  # этот url нужно заменить на url вашего Хероку приложения
+            return "?", 200
+
+
+        server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    else:
+        # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+        # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
         bot.remove_webhook()
-        bot.set_webhook(
-            url="https://choice-optimizer.herokuapp.com")  # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-
-
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+        bot.polling(none_stop=True)
