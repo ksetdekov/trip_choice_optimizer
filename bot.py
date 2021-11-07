5d3dd56d8b229ef60427bd22d4f81f58ba72a4c8
ksetdekov <ksetdekov@gmail.com>
2021-11-07 22:35:35 +0300

    i think this is extra

--- bot.py
+++ bot.py
@@ -24,14 +24,14 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda message: True, content_types=['text'])
+# def echo(message):
+#     bot.reply_to(message, message.text)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
