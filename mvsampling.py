e53eb381733545e8a50915ff219f5594fb77228f
Kirill Setdekov <ksetdekov@gmail.com>
2022-02-18 09:19:16 +0300

    more work on the class

--- mvsampling.py
+++ mvsampling.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import numpy as np
+from  scipy.stats import gamma, norm
 
 
 class HandsTable():
-    def __init__(self, options_list):
+    minimize = True
+    rho=3.37
+
+    def __init__(self, options_list, minimize=True, rho=None):
         self.hands = pd.DataFrame({'name': options_list,
                                    'mu': 0, 
                                    'Te': 0, 
                                    'alpha': 0.5, 
                                    'beta': 0.5
                                    })
+        self.minimize = minimize
+        if rho is not None:
+            self.rho = rho
+        
     @classmethod
     def to_minutes(self, timestr: str):
         '''
         convert timestr to float minutes
         '''
         return pd.to_timedelta(timestr).total_seconds()/60
- 
+    
     @classmethod
     def update_mean(self, X, T_last, mu_last):
         mu_new = T_last * mu_last / (T_last + 1) + X / (T_last + 1)
@@ -47,7 +55,6 @@ class HandsTable():
             raise ValueError('input time string or int/float value')
 
         _, mu, t, alpha, beta = self.hands[self.hands.name == name].values[0]
-        print(f'value{value}, mu{mu}, beta{beta}, t{t}')
         beta = HandsTable.update_rate(value, mu, beta, t)
         mu =  HandsTable.update_mean(value, t, mu)
         t = HandsTable.update_samples(t)
@@ -57,13 +64,45 @@ class HandsTable():
         self.hands.loc[self.hands.name == name, 'Te'] = t
         self.hands.loc[self.hands.name == name, 'alpha'] = alpha
         self.hands.loc[self.hands.name == name, 'beta'] = beta
+    
+    def grade(self):
+        hands_output = self.hands
+        tau = gamma.rvs( a = hands_output.alpha, scale=1/hands_output.beta)
+        theta_drops = norm.rvs(hands_output.mu, 1/hands_output.Te)
+        hands_output['tau'] = tau
+        hands_output['theta'] = theta_drops
+        hands_output['SD'] = np.sqrt(1/tau)
+
+
+
+        if self.minimize == True:
+            hands_output['var95'] = theta_drops + norm.ppf(1-0.05/2) * hands_output.SD
+            if hands_output.mu.min() == 0:
+                
+                output_df = hands_output.reindex(np.argsort(hands_output.Te))
+            else:
+                output_df = hands_output.reindex(np.argsort(self.rho * theta_drops + 1/tau))
+        else:
+            hands_output['var95'] = theta_drops + norm.ppf(0.05/2) * hands_output.SD
+            if hands_output.mu.min() == 0:
+                output_df = hands_output.reindex(np.argsort(hands_output.Te))
+            else:
+                output_df = hands_output.reindex(np.argsort(self.rho * theta_drops - 1/tau)[::-1])
+
+        return output_df
 
     def __str__(self):
         return repr(self.hands)
 
-a = HandsTable(['1, ', '2'])
+a = HandsTable(['1', '2'], minimize=False)
 
-print(a)
+# print(a)
 
 a.update_hands('2', 100)
-print(a)
+a.update_hands('2', 90)
+a.update_hands('1', 80)
+a.update_hands('1', 70)
+
+# print(a)
+
+print(a.grade())
