c0ba9df038c7c7bb36020d25018f21f79d750765
Kirill Setdekov <ksetdekov@gmail.com>
2023-01-08 15:59:07 +0300

    добавил начальный диалог

--- bot/creating_an_optimization.py
+++ bot/creating_an_optimization.py
@@ -1,37 +1,28 @@
+import sys
+import asyncio
+import logging
+from settings import BOT_TOKEN
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+from aiogram import Bot, Dispatcher, F, Router, html
+from typing import Any, Dict
 list_of_options = []
 optimization_direction = ["Минимизировать", "Максимизировать"]
 risk_tolerance_options = ["Стандартная", "Низкая", "Высокая"]
 # based on https://github.com/aiogram/aiogram/blob/dev-3.x/examples/finite_state_machine.py
-risk_tolerance_mapping = {"Стандартная":1, "Низкая":1/3.37, "Высокая":3.37}
-from typing import Any, Dict
+risk_tolerance_mapping = {"Стандартная": 1, "Низкая": 1/3.37, "Высокая": 3.37}
 
-from aiogram import Bot, Dispatcher, F, Router, html
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
-
-from settings import BOT_TOKEN
-
-import logging
-
-import asyncio
-import logging
-import sys
-from typing import Any, Dict
-
-from aiogram import Bot, Dispatcher, F, Router, html
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 form_router = Router()
+
 
 class Form(StatesGroup):
     name = State()
     options = State()
-    direction = State()
+    options_check = State()
+    maximize = State()
     risk_tolerance = State()
 
 
@@ -40,7 +31,7 @@ async def new_optimization(message: Message, state: FSMContext) -> None:
     await state.set_state(Form.name)
     await message.answer(
         "Что будем оптимизировать?",
-        reply_markup=ReplyKeyboardRemove(),
+        reply_markup=ReplyKeyboardRemove(remove_keyboard=True),
     )
 
 
@@ -58,85 +49,127 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     await state.clear()
     await message.answer(
         "Действие отменено.",
-        reply_markup=ReplyKeyboardRemove(),
+        reply_markup=ReplyKeyboardRemove(remove_keyboard=True),
     )
-
-
 
 
 @form_router.message(Form.name)
 async def process_name(message: Message, state: FSMContext) -> None:
-    await state.update_data(name=message.text)
     await state.set_state(Form.options)
-    await message.answer(
-        "Перечисли через запятую варианты ответов",
+    await state.update_data(name=message.text)
+
+    await message.reply(
+        "Спасибо\nА какие варианты существуют?\nПеречислите через запятую.",
         reply_markup=ReplyKeyboardRemove(remove_keyboard=True),
     )
-    data = await state.get_data()
-    logging.info(data)
-    await state.set_state(Form.options)
-    # await show_summary(message=message, data=data)
-
 
 
 @form_router.message(Form.options)
 async def process_options(message: Message, state: FSMContext) -> None:
-    data = await state.update_data(language=message.text)
-    await state.clear()
-    text = (
-        "Thank for all! Python is in my hearth!\nSee you soon."
-        if message.text.casefold() == "python"
-        else "Thank for information!\nSee you soon."
+    await state.update_data(options=message.text)
+    print('options')
+    if message.text is not None:
+        print(message.text.split())
+    data = await state.get_data()
+    list_of_options = data.get("options", None)
+    if list_of_options is not None:
+        list_of_options = list_of_options.split(',')
+        options_pretty = "\n".join(list_of_options)
+    else:
+        options_pretty = ''
+
+    await message.answer(
+        f"Спасибо, вы выбрали варианты :\n{options_pretty},\nСохранить и продолжить или ввести заново?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Continue"),
+                    KeyboardButton(text="Repeat"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
     )
-    await message.answer(text)
+    data = await state.get_data()
+    logging.info(data)
+    await state.set_state(Form.options_check)
+    # await show_summary(message=message, data=data)
+
+
+@form_router.message(Form.options_check, F.text.casefold() == "continue")
+async def process_dont_repeat_entry_options(message: Message, state: FSMContext) -> None:
+    # data = await state.get_data()
+    await message.answer(
+        "Спасибо, а мы будем искать вариант, который минимизует целевое значение или максимизирует?\nНапример, найти тот день, где меньше людей в бассейне - минимизировать,\nа найти самую вкусную кофету - максимизировать",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Минимизировать"),
+                    KeyboardButton(text="Максимизировать"),
+                ]
+            ],
+            resize_keyboard=True,
+        ))
+    await state.set_state(Form.maximize)
+    # await show_summary(message=message, data=data)
+
+
+@form_router.message(Form.options_check, F.text.casefold() == "repeat")
+async def process_repeat_entry_option(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.options)
+
+    await message.reply(
+        "Хорошо, введите варианты через замятую заново",
+        reply_markup=ReplyKeyboardRemove(remove_keyboard=True),
+    )
+
+
+@form_router.message(Form.maximize)
+async def process_options_minmax(message: Message, state: FSMContext) -> None:
+    print(message.text)
+    result_maximize = False
+    if (message.text is not None):
+        result_maximize = (message.text.lower() == 'Максимизировать')
+
+    await state.update_data(maximize=result_maximize)
+    await message.reply(
+        f"Хорошо, спасибо, вы выбрали вариант {message.text}\nТеперь вопрос про склонность к риску.",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Стандартная"),
+                    KeyboardButton(text="Низкая"),
+                    KeyboardButton(text="Высокая"),
+                ]
+            ],
+            resize_keyboard=True,
+        )
+    )
+    await state.set_state(Form.risk_tolerance)
+
+
+@form_router.message(Form.risk_tolerance)
+async def process_risk_tolerance_options(message: Message, state: FSMContext) -> None:
+    risk_tolerance = 'стандартная'
+    if message.text is not None:
+        risk_tolerance = message.text.lower()
+    await state.update_data(risk_tolerance=risk_tolerance)
+    await message.reply(
+        "Спасибо",
+        reply_markup=ReplyKeyboardRemove(remove_keyboard=True),
+    )
+
+    data = await state.get_data()
     await show_summary(message=message, data=data)
-    
-
-# @form_router.message(Form.like_bots, F.text.casefold() == "no")
-# async def process_dont_like_write_bots(message: Message, state: FSMContext) -> None:
-#     data = await state.get_data()
-#     await state.clear()
-#     await message.answer(
-#         "Not bad not terrible.\nSee you soon.",
-#         reply_markup=ReplyKeyboardRemove(),
-#     )
-#     await show_summary(message=message, data=data, positive=False)
 
 
-# @form_router.message(Form.like_bots, F.text.casefold() == "yes")
-# async def process_like_write_bots(message: Message, state: FSMContext) -> None:
-#     await state.set_state(Form.language)
-
-#     await message.reply(
-#         "Cool! I'm too!\nWhat programming language did you use for it?",
-#         reply_markup=ReplyKeyboardRemove(),
-#     )
-
-
-# @form_router.message(Form.like_bots)
-# async def process_unknown_write_bots(message: Message, state: FSMContext) -> None:
-#     await message.reply("I don't understand you :(")
-
-
-# @form_router.message(Form.language)
-# async def process_language(message: Message, state: FSMContext) -> None:
-#     data = await state.update_data(language=message.text)
-#     await state.clear()
-#     text = (
-#         "Thank for all! Python is in my hearth!\nSee you soon."
-#         if message.text.casefold() == "python"
-#         else "Thank for information!\nSee you soon."
-#     )
-#     await message.answer(text)
-#     await show_summary(message=message, data=data)
-
-
-async def show_summary(message: Message, data: Dict[str, Any], positive: bool = True) -> None:
+async def show_summary(message: Message, data: Dict[str, Any]) -> None:
     name = data["name"]
-    list_of_options = data.get("language", "<something unexpected>")
-    text = f"оптимизируем: {html.quote(name)}, "
-    
-    await message.answer(text=text, reply_markup=ReplyKeyboardRemove())
+    list_of_options = data.get("options")
+    logging.info(list_of_options)
+    text = f"оптимизируем: {html.quote(name)}, опции - {list_of_options}. Будет максимизация {data.get('maximize')}, Риск-толерантность {data.get('risk_tolerance')}"
+
+    await message.answer(text=text, reply_markup=ReplyKeyboardRemove(remove_keyboard=True))
 
 
 async def main():
