4e86cd0ebdba15097191c2bd5f64bf5c728e76fd
Kirill Setdekov <ksetdekov@gmail.com>
2025-02-04 20:07:27 +0300

    added loading from dotenv

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import os
 from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Get bot token from environment variable
 BOT_TOKEN = os.getenv("BOT_TOKEN")
