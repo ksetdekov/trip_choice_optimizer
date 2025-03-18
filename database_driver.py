8da5c15fc8fb1a673042a78db7b1f8a964ddca53
Kirill Setdekov <ksetdekov@gmail.com>
2025-03-18 13:05:28 +0300

    refactor database schema to enforce foreign key constraints and streamline user handling

--- database_driver.py
+++ database_driver.py
@@ -6,39 +6,71 @@ class DatabaseDriver:
         self.db_name = db_name
         self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
+        self.cursor.execute("PRAGMA foreign_keys = ON")  # Enable foreign key enforcement
         self.create_tables()
 
     def create_tables(self):
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS user_optimization (
-                optimization_name TEXT,
-                change_datetime DATETIME,
-                user_id INTEGER
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                telegram_user_id INTEGER NOT NULL UNIQUE
             )
         ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_optimization (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                optimization_name TEXT NOT NULL,
+                change_datetime DATETIME,
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE            
+            )
+        ''')
+
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS optimization_variant (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                optimization_name TEXT,
-                variant_name TEXT,
+                optimization_id INTEGER NOT NULL,
+                variant_name TEXT NOT NULL,
                 change_datetime DATETIME,
-                user_id INTEGER
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY (optimization_id) REFERENCES user_optimization(id) ON DELETE CASCADE
             )
         ''')
-        # Renamed table: optimization_samples now stores the option value given by the user.
+
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS optimization_samples (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                optimization_name TEXT,
-                variant_name TEXT,
+                optimization_id INTEGER NOT NULL,
+                variant_id INTEGER NOT NULL,
                 option_value NUMERIC,
                 change_datetime DATETIME,
-                user_id INTEGER
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY (optimization_id) REFERENCES user_optimization(id) ON DELETE CASCADE,
+                FOREIGN KEY (variant_id) REFERENCES optimization_variant(id) ON DELETE CASCADE
             )
         ''')
         self.conn.commit()
 
-    def add_optimization(self, optimization_name, user_id):
+    def add_optimization(self, optimization_name, telegram_user_id):
+        # Ensure the user exists or insert them
+        self.cursor.execute(
+            "SELECT id FROM users WHERE telegram_user_id = ?",
+            (telegram_user_id,)
+        )
+        user_row = self.cursor.fetchone()
+        if user_row is None:
+            self.cursor.execute(
+                "INSERT INTO users (telegram_user_id) VALUES (?)",
+                (telegram_user_id,)
+            )
+            self.conn.commit()
+            user_id = self.cursor.lastrowid
+        else:
+            user_id = user_row[0]
+
         change_datetime = datetime.now()
         self.cursor.execute(
             '''
@@ -70,24 +102,56 @@ class DatabaseDriver:
         return self.cursor.fetchall()
     
     def add_variant(self, optimization_name, variant_name, user_id):
+        '''
+        user_optimization
+
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                optimization_id INTEGER NOT NULL,
+                variant_name TEXT NOT NULL,
+                change_datetime DATETIME,
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY (optimization_id) REFERENCES user_optimization(id) ON DELETE CASCADE 
+        '''
         change_datetime = datetime.now()
+        # get if of optimization based on optimization_name
         self.cursor.execute(
             '''
-            INSERT INTO optimization_variant (optimization_name, variant_name, change_datetime, user_id)
+            SELECT id, user_id
+            FROM user_optimization
+            WHERE optimization_name = ? AND user_id = ?
+            ''',
+            (optimization_name, user_id)
+        )
+        optimization_id, internal_user_id = self.cursor.fetchone()
+
+
+        self.cursor.execute(
+            '''
+            INSERT INTO optimization_variant (optimization_id, variant_name, change_datetime, user_id)
             VALUES (?, ?, ?, ?)
             ''',
-            (optimization_name, variant_name, change_datetime, user_id)
+            (optimization_id, variant_name, change_datetime, internal_user_id)
         )
         self.conn.commit()
     
     def get_variants(self, optimization_name, user_id):
         self.cursor.execute(
             '''
-            SELECT variant_name, change_datetime
-            FROM optimization_variant
+            SELECT id, user_id
+            FROM user_optimization
             WHERE optimization_name = ? AND user_id = ?
             ''',
             (optimization_name, user_id)
+        )
+        optimization_id, internal_user_id = self.cursor.fetchone()
+        self.cursor.execute(
+            '''
+            SELECT variant_name, change_datetime
+            FROM optimization_variant
+            WHERE optimization_id = ? AND user_id = ?
+            ''',
+            (optimization_id, internal_user_id)
         )
         return self.cursor.fetchall()
     
@@ -96,29 +160,21 @@ class DatabaseDriver:
         Remove a specific optimization by its name and user ID,
         and also remove all associated variants and samples.
         """
-        # First, remove all variants associated with the optimization.
         self.cursor.execute(
             '''
-            DELETE FROM optimization_variant
+            SELECT id, user_id
+            FROM user_optimization
             WHERE optimization_name = ? AND user_id = ?
             ''',
             (optimization_name, user_id)
         )
-        # Remove any samples associated with the optimization.
-        self.cursor.execute(
-            '''
-            DELETE FROM optimization_samples
-            WHERE optimization_name = ? AND user_id = ?
-            ''',
-            (optimization_name, user_id)
-        )
-        # Then, remove the optimization itself.
+        optimization_id, internal_user_id = self.cursor.fetchone()
         self.cursor.execute(
             '''
             DELETE FROM user_optimization
-            WHERE optimization_name = ? AND user_id = ?
+            WHERE id = ? AND user_id = ?
             ''',
-            (optimization_name, user_id)
+            (optimization_id, internal_user_id)
         )
         self.conn.commit()
     
